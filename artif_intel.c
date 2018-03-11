67baa2b7f359ed1ac27561687ab7b38eb6e2d0fe
Steeven MORTIER <smortier@e3r1p8.42.fr>
2018-03-11 15:52:03 +0100

    Bon courage

--- artif_intel.c
+++ artif_intel.c
@@ -2,16 +2,17 @@
 
 void	artif_intel(t_informations *info)
 {
-	static int		swap;
+	static int		i_got_u;
+
 //	if (!info->pos_reached) // la j ai une premiere target
 //		reach_position(info);
-	/*else */ if (info->p_height > 1)
+	i_got_u = 0;
+	/*else */ if (/*info->p_height > 1 && */!i_got_u)
 	{
-		info->target[0] = info->his_first_pos[0] - 3;
+		info->target[0] = (info->his_first_pos[0] >= info->map_height / 2) ? info->his_first_pos[0] - 3 : info->his_first_pos[0] + 3;
 		info->target[1] = info->his_first_pos[1];
-		swap = 0;
 		reach_position(info);
-	}
+	}//un truc comme ca
 	/*else if (info->map[info->map_height - 1][0] == '.')
 	{
 		info->target[0] = info->map_height - 1;
@@ -22,8 +23,9 @@ void	artif_intel(t_informations *info)
 	{
 		info->target[0] = info->his_first_pos[0] + 3;
 		info->target[1] = info->his_first_pos[1];
-		swap = 1;
 		reach_position(info);
 	}
+//	dprintf(2, "\e[34mi_got_u = [%d] TARGET = [%d][%d]\n\e[0m", i_got_u, info->target[0], info->target[1]);
+//	sleep(1);
 	//jvais try de faire un truc qui preshot sa target et l'intercepter =}
 }
